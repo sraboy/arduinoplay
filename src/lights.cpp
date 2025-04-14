d8677afb9299e676474ce8c8f6ed42ea0be13dd5
Steven Lavoie <steven.lavoie@gmail.com>
2025-04-14 08:48:23 -0500

    Needless overengineer note playing and pins with speaker

--- src/lights.cpp
+++ src/lights.cpp
@@ -1,48 +1,124 @@
 #include "pitches.h"
 #include <Arduino.h>
 
-const struct _leds {
+const struct _pins {
     const int8_t blue = 13;
     const int8_t red = 12;
     const int8_t yellow = 11;
     const int8_t green = 10;
-} leds;
+    const int8_t speaker = 9;
+} pins;
 
-void lights_on();
+class notes {
+    private: 
+        notes() {}
+        ~notes() {}
+    public:
+        static void setup_pins() {
+            pinMode(pins.blue, OUTPUT);
+            pinMode(pins.yellow, OUTPUT);
+            pinMode(pins.green, OUTPUT);
+            pinMode(pins.red, OUTPUT);
+        }
+        
+        static void play_blue() {
+            digitalWrite(pins.blue, HIGH);
+            delay(100);
+            digitalWrite(pins.blue, LOW);
+
+            tone(pins.speaker, 1000, 100);
+            delay(100);
+            noTone(pins.speaker);
+        }
+
+        static void play_red() {
+            digitalWrite(pins.red, HIGH);
+            delay(100);
+            digitalWrite(pins.red, LOW);
+
+            tone(pins.speaker, 2000, 100);
+            delay(100);
+            noTone(pins.speaker);
+        }
+        static void play_yellow() {
+            digitalWrite(pins.yellow, HIGH);
+            delay(100);
+            digitalWrite(pins.yellow, LOW);
+
+            tone(pins.speaker, 3000, 100);
+            delay(100);
+            noTone(pins.speaker);
+        }
+        static void play_green() {
+            digitalWrite(pins.green, HIGH);
+            delay(100);
+            digitalWrite(pins.green, LOW);
+
+            tone(pins.speaker, 4000, 100);
+            delay(100);
+            noTone(pins.speaker);
+        }
+        static void play_all() {
+            digitalWrite(pins.blue, HIGH);
+            digitalWrite(pins.red, HIGH);
+            digitalWrite(pins.yellow, HIGH);
+            digitalWrite(pins.green, HIGH);
+
+            tone(pins.speaker, 5000, 100);
+            delay(100);
+            noTone(pins.speaker);
+
+            digitalWrite(pins.blue, LOW);
+            digitalWrite(pins.red, LOW);
+            digitalWrite(pins.yellow, LOW);
+            digitalWrite(pins.green, LOW);
+        }
+};
+
+void all_lights_on();
 void new_round();
 
 void lights_setup() {
-    pinMode(leds.blue, OUTPUT);
-    pinMode(leds.yellow, OUTPUT);
-    pinMode(leds.green, OUTPUT);
-    pinMode(leds.red, OUTPUT);
+    notes::setup_pins();
 }
 
 void lights_loop() {
     new_round();
+
+    notes::play_blue();
+    delay(500);
+    notes::play_red();
+    delay(500);
+    notes::play_yellow();
+    delay(500);
+    notes::play_green();
+    delay(500);
+    notes::play_all();
+    delay(500);
+    all_lights_on();
 }
 
 void new_round() {
     // rotate through lights
     for (int i = 0; i < 4; i++) {
-        digitalWrite(leds.blue, HIGH);
+        digitalWrite(pins.blue, HIGH);
         delay(100);
-        digitalWrite(leds.blue, LOW);
-        digitalWrite(leds.red, HIGH);
+        digitalWrite(pins.blue, LOW);
+        digitalWrite(pins.red, HIGH);
         delay(100);
-        digitalWrite(leds.red, LOW);
-        digitalWrite(leds.yellow, HIGH);
+        digitalWrite(pins.red, LOW);
+        digitalWrite(pins.yellow, HIGH);
         delay(100);
-        digitalWrite(leds.yellow, LOW);
-        digitalWrite(leds.green, HIGH);
+        digitalWrite(pins.yellow, LOW);
+        digitalWrite(pins.green, HIGH);
         delay(100);
-        digitalWrite(leds.green, LOW);
+        digitalWrite(pins.green, LOW);
     }
 }
 
-void lights_on() {
-    digitalWrite(leds.blue, HIGH);
-    digitalWrite(leds.yellow, HIGH);
-    digitalWrite(leds.green, HIGH);
-    digitalWrite(leds.red, HIGH);
+void all_lights_on() {
+    digitalWrite(pins.blue, HIGH);
+    digitalWrite(pins.yellow, HIGH);
+    digitalWrite(pins.green, HIGH);
+    digitalWrite(pins.red, HIGH);
 }
